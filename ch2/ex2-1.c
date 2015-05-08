16fc26b0d69419722e1341eea538f5413e5c2c3a
Wally <wally@imwally.net>
2015-05-07 23:41:19 -0400

    spelling

--- ch2/ex2-1.c
+++ ch2/ex2-1.c
@@ -1,7 +1,7 @@
 /* Exercise 2-1
  *
  * Write a program to determine the ranges of char, short, int, and
- * long varibles, both signed and unsigned, by printing apporpriate
+ * long varibles, both signed and unsigned, by printing appropriate
  * values from standard headers and by direct computation. Harder if
  * you compute them: determine the ranges of the various
  * floating-point types.
