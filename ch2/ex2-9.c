fc151fa0ade18a8f2fd0783f1f3c3446f11ad41b
Wally <wally@imwally.net>
2015-05-18 13:26:22 -0400

    add explanation and swap for to while loop

--- ch2/ex2-9.c
+++ ch2/ex2-9.c
@@ -3,6 +3,30 @@
  * In a two's complement number system, x &= (x-1) deletes the
  * rightmost 1-bit in x. Explain why. Use this observation to write a
  * faster version of bitcount.
+ *
+ * Explanation:
+ * 
+ * In a two's complement number system the right most bit denotes the
+ * 1's place. This bit will never be occupied in an even number:
+ *
+ * 0010 = 2
+ * 0100 = 4
+ * 0110 = 6
+ *
+ * If the number happens to be odd:
+ *
+ * 0001 = 1
+ * 0011 = 3
+ * 0101 = 5
+ * 0111 = 7
+ *
+ * Then subtracting 1 and &'ing it with itself will drop the rightmost
+ * bit. Repeating this process in a loop until x = 0 will count every
+ * 1 bit in x.
+ *
+ * 1. 0111 & 0110 = 0110
+ * 2. 0110 & 0101 = 0100
+ * 3. 0100 & 0011 = 0000
  */
 
 #include <stdio.h>
@@ -12,7 +36,7 @@ int fastbitcount(unsigned x);
 
 int main() {
 
-    unsigned x = 227;
+    unsigned x = 238;
 
     printf("%d\n", bitcount(x));
     printf("%d\n", fastbitcount(x));
@@ -23,9 +47,10 @@ int main() {
 /* fastbitcount: count 1 bits in x faster */
 int fastbitcount(unsigned x) {
 
-    int b;
-    for (b = 1; (x &= (x-1)) != 0; b++) {
-	;
+    int b = 0;
+    while (x != 0) {
+	x &= (x-1);
+	b++;
     }
 
     return b;
