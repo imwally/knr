2311d12647287e8a5196e523fe5905f84f951e0b
Wally <wally@imwally.net>
2015-05-19 17:14:44 -0400

    this shit needs to be fixed

---
+++ ch3/ex3-2.c
@@ -0,0 +1,48 @@
+/* Exercise 3-2
+ *
+ * Write a fucniton escape(s,t) that converts characters like newline
+ * and tab into visible escape sequences like \n and \t as it copies
+ * the string t to s. Use a switch. Write a function for the other
+ * direction as well, converting escape sequences into the real
+ * characters.
+ */
+
+#include <stdio.h>
+
+#define MAX 1000
+
+void escape(char s[], char t[]);
+
+int main() {
+
+    char msg[MAX] = "This is my\nmessage that\nhas new lines\tand\tsome tabs in it.";
+    char escaped[MAX];
+
+    escape(msg, escaped);
+    printf("message:\n%s\n\n", msg);
+    printf("escaped:\n%s\n", escaped);
+
+}
+
+void escape(char s[], char t[]) {
+
+    int i, j;
+
+    for(i = j =0; s[i] != '\0'; ++i) {
+	
+	switch(s[i]) {
+	case '\t':
+	    t[j++] = '\\';
+	    t[j++] = 't';
+	    ++i;
+	case '\n':
+	    t[j++] = '\\';
+	    t[j++] = 'n';
+	    ++i;
+	default:
+	    t[j++] = s[i];
+	}
+    }
+
+    t[j] = '\0';
+}
