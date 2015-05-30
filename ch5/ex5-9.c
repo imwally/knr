89bcbab2030aa6fb9d4240c68c034abb00a8625c
Wally <wally@imwally.net>
2015-05-29 22:06:16 -0400

    add ex5-9

---
+++ ch5/ex5-9.c
@@ -0,0 +1,81 @@
+/* Exercise 5-9
+ *
+ * Rewrite the routines of day_of_year and month_day with pointers
+ * instead of indexing.
+ */
+
+#include <stdio.h>
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int *pmonth, int *pday);
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int main(void) {
+
+    int m, d;
+
+    /* day_of_year */
+    printf("day_of_year(2015, 5, 29): %d\n", day_of_year(2015, 5, 29));
+    printf("day_of_year(2015, 14, 29): %d\n", day_of_year(2015, 14,29));
+    printf("day_of_year(2015, 5, 32): %d\n", day_of_year(2015, 5, 32));
+
+    /* month_day */
+    month_day(2015, 366, &m, &d);
+    printf("month_day(2015, 366, &m, &d): m: %d d: %d\n", m, d);    
+
+    month_day(2016, 366, &m, &d);
+    printf("month_day(2016, 366, &m, &d): m: %d d: %d\n", m, d);    
+    
+    month_day(2015, 149, &m, &d);
+    printf("month_day(2015, 149, &m, &d): m: %d d: %d\n", m, d);
+}
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day) {
+
+    int i, leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+
+    char *p = daytab[leap]+1;
+
+    /* make sure months are 1-12 and days are 1-31 */
+    if (month > 0 && month <= 12 && day > 0 && day <= 31) {
+    
+	for (i = 1; i < month; i++, *p++) {
+	    day += *p;
+	}
+	
+	return day;
+    }
+
+    else {
+	return -1;
+    }
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday) {
+
+    int i, leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+
+    char *p = daytab[leap]+1;
+
+    /* make sure yearday is 1-365 or 1-366 for leap years */
+    if (yearday > 0 && yearday <= 365 || leap > 0 && yearday > 0 && yearday <=366) {
+	for (i = 1; yearday > *p; i++, *p++) {
+	    yearday -= *p;
+	}
+	*pmonth = i;
+	*pday = yearday;
+    }
+
+    else {
+	*pmonth = -1;
+	*pday = -1;
+    }
+}
